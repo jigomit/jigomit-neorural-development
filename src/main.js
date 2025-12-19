3cfd663dd332355fa05acb51d3ffe228b8708a12
dhaval48 <info@ongoingcloud.com>
2025-12-19 12:27:00 +0530

    improve performance

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+// Performance: Import CSS - Vite will handle it, but we'll optimize loading
 import '@/assets/main.css';
 import { useSEO } from './composables/useSEO';
 
